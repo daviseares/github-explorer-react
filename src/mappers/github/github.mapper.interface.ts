17badd7e66121105f4253f7bff9a8b2bce0c69c3
davi.seares <davi.seares@premiersoft.net>
2024-03-14 20:40:20 -0300

    remove comments

--- src/mappers/github/github.mapper.interface.ts
+++ src/mappers/github/github.mapper.interface.ts
@@ -6,7 +6,6 @@ import {
 } from '~/utils/types';
 
 export interface IGithubMapper {
-  // replace any with the correct type
   mapRepository: (repository: RepoFromApi[]) => RepositoryType[];
   mapIssue: (issue: IssueFromApi[]) => Issue[];
 }
